03e4914ddd6727f30d60934dc5c037a6d731eb6f
wengweng85 <whengshaohui@163.com>
2019-01-10 13:59:21 +0800

    config

---
+++ sxjy-cloud-config-client/src/main/java/com/insigma/cloud/config/CloudConfigClientApplication.java
@@ -0,0 +1,12 @@
+package com.insigma.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigClientApplication.class, args);
+	}
+}
