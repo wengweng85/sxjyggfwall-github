58dd3b68b0c20b1c31d5792210b3c135cfbd4024
wengweng85 <whengshaohui@163.com>
2019-03-22 16:33:27 +0800

    sxjy-gateway

--- sxjy-cloud-gateway/src/main/java/com/insigma/cloud/gateway/CloudGateWayApplication.java
+++ sxjy-cloud-gateway/src/main/java/com/insigma/cloud/gateway/CloudGateWayApplication.java
@@ -1,10 +1,13 @@
 package com.insigma.cloud.gateway;
 
+import com.insigma.cloud.gateway.filter.ElapsedFilter;
+import com.insigma.cloud.gateway.filter.TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.filter.factory.StripPrefixGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -19,14 +22,23 @@ public class CloudGateWayApplication {
     }
 
 
-    //@Bean
+    @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         StripPrefixGatewayFilterFactory.Config config = new StripPrefixGatewayFilterFactory.Config();
         config.setParts(1);
         return builder.routes()
-                .route("host_route", r -> r.path("/a/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8081"))
-                .route("host_route", r -> r.path("/b/**").filters(f -> f.stripPrefix(1)).uri("http://localhost:8082"))
-                .build();
+            .route("host_route", r -> r.path("/about/**").filters(f -> f.stripPrefix(1).filter(new ElapsedFilter()).addResponseHeader("X-Response-Default-Foo", "Default-Bar")).uri("http://ityouknow.com"))
+            .build();
+    }
+
+    @Bean
+    public ElapsedFilter elapsedFilter(){
+        return new ElapsedFilter();
+    }
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
     }
 
 
---
+++ sxjy-cloud-gateway/src/main/java/com/insigma/cloud/gateway/filter/TokenFilter.java
@@ -0,0 +1,28 @@
+package com.insigma.cloud.gateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 全局过滤器
+ */
+public class TokenFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String token = exchange.getRequest().getQueryParams().getFirst("token");
+        if (token == null || token.isEmpty()) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
